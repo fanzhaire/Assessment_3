52935b84e7cb767fdb40aa72a0f6f0b5c5c414c1
fanzhaire <fan990708@gmail.com>
2023-09-25 13:47:26 +1000

    Fix the bug of play button playing animation

--- Assets/Scripts/TriggerAnimations.cs
+++ Assets/Scripts/TriggerAnimations.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerAnimations : MonoBehaviour
 {
@@ -10,49 +9,65 @@ public class TriggerAnimations : MonoBehaviour
     public Animator scaredStateAnimator;
     public Animator recoveringStateAnimator;
     public Animator deadStateAnimator;
-
-    private bool animationTriggered = false;  
+    public Button playButton;
 
     void Start()
     {
-        DisableAutoPlay();
+        DisableAllAnimators();
+
+        if (playButton != null)
+        {
+            playButton.onClick.AddListener(OnPlayButtonClicked);
+        }
     }
 
-    void DisableAutoPlay()
+    void DisableAllAnimators()
     {
-        if (pacStudentAnimator != null) pacStudentAnimator.StopPlayback();
-        if (powerPelletsAnimator != null) powerPelletsAnimator.StopPlayback();
-        if (walkingStateAnimator != null) walkingStateAnimator.StopPlayback();
-        if (scaredStateAnimator != null) scaredStateAnimator.StopPlayback();
-        if (recoveringStateAnimator != null) recoveringStateAnimator.StopPlayback();
-        if (deadStateAnimator != null) deadStateAnimator.StopPlayback();
+        if (pacStudentAnimator != null) pacStudentAnimator.enabled = false;
+        if (powerPelletsAnimator != null) powerPelletsAnimator.enabled = false;
+        if (walkingStateAnimator != null) walkingStateAnimator.enabled = false;
+        if (scaredStateAnimator != null) scaredStateAnimator.enabled = false;
+        if (recoveringStateAnimator != null) recoveringStateAnimator.enabled = false;
+        if (deadStateAnimator != null) deadStateAnimator.enabled = false;
     }
 
     public void OnPlayButtonClicked()
     {
-        if (!animationTriggered)  
+        PlayAnimations();
+    }
+
+    void PlayAnimations()
+    {
+        if (pacStudentAnimator != null)
         {
-            EnableAndPlayAnimators();
-            animationTriggered = true;  
+            pacStudentAnimator.enabled = true;
+            pacStudentAnimator.Play("WalkUp");  
         }
-    }
-
-    void EnableAndPlayAnimators()
-    {
-        TriggerAnimator(pacStudentAnimator);
-        TriggerAnimator(powerPelletsAnimator);
-        TriggerAnimator(walkingStateAnimator);
-        TriggerAnimator(scaredStateAnimator);
-        TriggerAnimator(recoveringStateAnimator);
-        TriggerAnimator(deadStateAnimator);
-    }
-
-    void TriggerAnimator(Animator animator)
-    {
-        if (animator != null)
+        if (powerPelletsAnimator != null)
         {
-            animator.enabled = true;  
-            animator.SetTrigger("PlayAnimation");  
+            powerPelletsAnimator.enabled = true;
+            powerPelletsAnimator.Play("PowerPelletBlink");
+        }
+        if (walkingStateAnimator != null)
+        {
+            walkingStateAnimator.enabled = true;
+            walkingStateAnimator.Play("GhostWalk");
+        }
+        if (scaredStateAnimator != null)
+        {
+            scaredStateAnimator.enabled = true;
+            scaredStateAnimator.Play("A Scared state");
+        }
+        if (recoveringStateAnimator != null)
+        {
+            recoveringStateAnimator.enabled = true;
+            recoveringStateAnimator.Play("A Recovering state");
+        }
+        if (deadStateAnimator != null)
+        {
+            deadStateAnimator.enabled = true;
+            deadStateAnimator.Play("A Dead state");
         }
     }
 }
+
